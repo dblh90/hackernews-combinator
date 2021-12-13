b2fabaa3fe17d5ddd67011215cd142b19f56ba53
doubleH90 <hamzahasan1990@gmail.com>
2021-12-13 12:12:39 +0200

    Add check if package is installed

--- script/news-combinator.sh
+++ script/news-combinator.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+# Required packages
+REQUIRED_PKG=jq
+
+# Check if required packages are installed?
+package_installed=$(dpkg-query --list | grep -w "${REQUIRED_PKG}")
+
+if [ -z "$package_installed" ]; then
+    echo "Installing required packages ${REQUIRED_PKG}"
+    sudo apt install $REQUIRED_PKG
+fi
+
 top_story_id=$(curl -s 'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty' | jq '.[0]')
 
 top_story_url='https://hacker-news.firebaseio.com/v0/item/'${top_story_id}'.json?print=pretty'
